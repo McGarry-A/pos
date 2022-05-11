879d448665d8799ce7d347e43245c7719401470a
Ahmed McGarry <atomcgarry@gmail.com>
2022-05-11 17:03:42 +0100

    context

--- src/Context/BasketProvider.tsx
+++ src/Context/BasketProvider.tsx
@@ -1,5 +1,4 @@
-import { title } from "process";
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { ProductInterface } from ".";
 import { BasketInterface } from "../Components/Basket";
 
@@ -8,32 +7,35 @@ type RemoveItemParams = { id: string; quantity?: number };
 type ClearItemParams = { id: string };
 
 type BasketContextType = {
-  basket: BasketInterface;
+  basketContext: BasketInterface;
   addItem: (params: AddItemParams) => void;
   removeItem: (params: RemoveItemParams) => void;
   clearItem: (params: ClearItemParams) => void;
   clearBasket: () => void;
 };
 
-const BasketContext = createContext<BasketContextType>({
-  basket: null,
-  addItem: () => null,
-  removeItem: () => null,
-  clearItem: () => null,
-  clearBasket: () => null,
-});
+const BasketContext = createContext<BasketContextType | void>(
+  {} as BasketContextType
+);
 
 export const useBasket = () => useContext(BasketContext);
 
 export const BasketProvider = ({ children }: { children: React.ReactNode }) => {
-  const addItem = () => {};
+  const [basketContext, setBasketContext] = useState({} as BasketInterface);
+  const addItem = (item) => {
+    const { items } = basketContext;
+    const newItems = [...items, item];
+    const newBasket = { ...basketContext };
+    newBasket.items = newItems;
+    setBasketContext(newBasket);
+  };
   const removeItem = () => {};
   const clearItem = () => {};
   const clearBasket = () => {};
 
   return (
     <BasketContext.Provider
-      value={{ basket, addItem, removeItem, clearBasket, clearItem }}
+      value={{ basketContext, addItem, removeItem, clearBasket, clearItem }}
     >
       {children}
     </BasketContext.Provider>
